16a35596e8acfb103c3794cd4a46681f44ba3bfb
Charles Jarvis <csjarvis43@gmail.com>
2022-07-20 11:07:40 -0600

    commit tree d11113df08274d539081b781c0359783a4fe57e8

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    punct_count = self.split(/\.|!|\?/)
+    punct_count.filter { |sentence| !sentence.empty? }.size
   end
 end
